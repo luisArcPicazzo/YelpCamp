74716497d352be79f689839383778fb40a32fefd
luisArcPicazzo <luis.arc.picazzo@me.com>
2021-09-17 20:10:56 -0500

    STABLE - joi validation new Camp

--- joiValidationSchemas.js
+++ joiValidationSchemas.js
@@ -1,14 +1,27 @@
-const Joi = require('joi');
+//const Joi = require('joi');
 const joi = require ('joi');
 
+//module.exports.joiCampgroundSchema = 
+//Joi.object({
+//    newCampground: 
+//        Joi.object({
+//            title: Joi.string().required(),
+//            image: Joi.string().required(),
+//            price: Joi.number().required().min(0.0),
+//            description: Joi.string().required(),
+//            location: Joi.string().required()
+//        }).required() // newCampground comes from new.ejs 
+//})
+
+
 module.exports.joiCampgroundSchema = 
-Joi.object({
+joi.object({
     newCampground: 
-        Joi.object({
-            title: Joi.string().required(),
-            image: Joi.string().required(),
-            price: Joi.number().required().min(0.0),
-            description: Joi.string().required(),
-            location: Joi.string().required()
+        joi.object({
+            title: joi.string().required(),
+            image: joi.string().required(),
+            price: joi.number().required().min(0.0),
+            description: joi.string().required(),
+            location: joi.string().required()
         }).required() // newCampground comes from new.ejs 
 })
