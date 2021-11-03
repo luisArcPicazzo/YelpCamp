287b7e972352f16bb540dd4ee9ae6bf25afcbd3c
Eduardo Picazzo <luis.arc.picazzo@me.com>
2021-11-03 02:01:07 -0600

    sctn-55, vd-547 fixed seeds with location data

--- seeds/index.js
+++ seeds/index.js
@@ -31,6 +31,7 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const randomPrice = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground ({
+            // your user ID
             author: '6175219bb14bcdc119cc4b9e',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${getSampleTitle(descriptors)} ${getSampleTitle(places)}`, // weird syntax but cool one to master
@@ -40,20 +41,11 @@ const seedDB = async() => {
             necessitatibus maxime voluptatibus. Labore commodi porro repellendus quos ipsum voluptate deserunt saepe expedita impedit dolore praesentium itaque dicta \
             provident nisi inventore magnam ea, excepturi cupiditate. Provident, delectus iste!',
             price: randomPrice,
+            geometry: {
+              type: "Point",
+              coordinates: [-113.1331, 47.0202]
+            },
             images: [
-              //  {
-              //    url: 'https://res.cloudinary.com/e-pc/image/upload/v1635096294/YelpCamp/l0tf2dcnjgw4shjib4wj.jpg',
-              //    filename: 'YelpCamp/l0tf2dcnjgw4shjib4wj'
-              //  },
-              //  {
-              //    url: 'https://res.cloudinary.com/e-pc/image/upload/v1635096296/YelpCamp/j2gyevb1c9suu26xrc8k.jpg',
-              //    filename: 'YelpCamp/j2gyevb1c9suu26xrc8k'
-              //  },
-              //  {
-              //    url: 'https://res.cloudinary.com/e-pc/image/upload/v1635096297/YelpCamp/qjuqyfjoo86miz2k4hvt.jpg',
-              //    filename: 'YelpCamp/qjuqyfjoo86miz2k4hvt'
-              //  }
-
                 {
                   url: 'https://res.cloudinary.com/e-pc/image/upload/v1635749462/YelpCamp/rqrponndxnvwscpqhg36.jpg',
                   filename: 'YelpCamp/rqrponndxnvwscpqhg36'
