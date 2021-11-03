2aab3e9e424c53c88249bb419669f3711a18689d
Eduardo Picazzo <luis.arc.picazzo@me.com>
2021-11-03 02:10:05 -0600

    sctn-55, vd-548 added map pop up title/location

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,10 +3,19 @@
 mapboxgl.accessToken = mapBoxTokenFromEjsShowTemplate;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL can change the map style/theme here!. Look it up in the docs
         center: campgroundMapLocation.geometry.coordinates, // starting position [lng, lat]
         zoom: 9 // starting zoom
     });
 
 // Create a default Marker and add it to the map.
-new mapboxgl.Marker().setLngLat(campgroundMapLocation.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+    .setLngLat(campgroundMapLocation.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup( { offset: 25 } )
+                    .setHTML(
+                        `<h5>${campgroundMapLocation.title}</h5>
+                         <p>${campgroundMapLocation.location}</p>`
+                    )
+    )
+    .addTo(map);
